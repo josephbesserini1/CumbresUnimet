8b924e6f6179ac4769a0f17eb76cf7018c9fd896
Eteran07 <edgar.teran@correo.unimet.edu.ve>
2025-03-10 00:43:40 -0400

    Primer Commit, MP2

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <> 
+    
+    <div className="text-[25px] text-primary">Hola Mundo</div>
+    <div className="text-[25px] text-primary">Hola Mundo</div>
+    <div className="text-[25px] text-primary">Hola Mundo</div>
+    <div className="text-[25px] text-primary">Hola Mundo</div>
+    <div className="text-[25px] text-secondary">Hola Mundo</div>
+     </>
+    
+  );
+}
